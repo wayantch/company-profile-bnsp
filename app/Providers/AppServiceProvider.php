80c594f7be8e205981e403e4477c8ccac0f4afa0
wayantch <yansskuy123@gmail.com>
2026-05-29 01:07:50 +0700

    fix: rebuild assets with https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
         Vite::prefetch(concurrency: 3);
     }
 }
